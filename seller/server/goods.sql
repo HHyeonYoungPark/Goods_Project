073c89745cc320d48ccf6514ce3166dc851d71e2
Hyeonyoung Park <aud8637282@naver.com>
2022-11-04 16:00:27 +0900

    regist, sql update

--- seller/server/goods.sql
+++ seller/server/goods.sql
@@ -5,23 +5,23 @@ CREATE TABLE item(
 idx INT AUTO_INCREMENT PRIMARY KEY,
 category VARCHAR(50),
 image VARCHAR(255),
-NAME VARCHAR(100),
+itemname VARCHAR(100),
 price VARCHAR(50),
 stock INT,
 contents TEXT,
 regdate DATE
-)
+);
 
 CREATE TABLE seller(
 idx INT AUTO_INCREMENT PRIMARY KEY,
 id VARCHAR(50) UNIQUE,
 pw VARCHAR(255),
-NAME VARCHAR(10),
+sellername VARCHAR(10),
 email varchar(255),
-TYPE VARCHAR(10),
-image VARCHAR(255),
+sellertype VARCHAR(10),
 channelname VARCHAR(100),
 url VARCHAR(255),
+sellerimage VARCHAR(255),
 intro VARCHAR(100),
 regdate DATE 
-)
+);
