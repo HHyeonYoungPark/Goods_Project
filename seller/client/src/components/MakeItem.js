cc24e3e18a1f4e587e7568486862b6ed9b9c6c1a
Hyeonyoung Park <aud8637282@naver.com>
2022-11-04 11:06:21 +0900

    module init & folder,file create

---
+++ seller/client/src/components/MakeItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MakeItem() {
+  return <div>MakeItem</div>;
+}
+
+export default MakeItem;
---
+++ seller/client/src/components/Regist.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Regist() {
+  return <div>Regist</div>;
+}
+
+export default Regist;
