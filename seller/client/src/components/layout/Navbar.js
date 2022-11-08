a378277b550f900e56edc01438b88ce52bce036d
sauring3 <jeonghunhijohn@gmail.com>
2022-11-08 23:36:29 +0900

    111

--- seller/client/src/components/layout/Navbar.js
+++ seller/client/src/components/layout/Navbar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "../css/layout/Navbar.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="container">
+      <div className="L-nav">
+        <div>굿즈제작</div>
+        <div>상품등록</div>
+        <div>판매현황</div>
+      </div>
+      <div className="R-nav">
+        <div>고객센터</div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
