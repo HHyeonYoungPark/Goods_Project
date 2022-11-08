7d28aaba39d27c8e1acb2095c828cd987c76d27c
sauring3 <jeonghunhijohn@gmail.com>
2022-11-08 13:30:23 +0900

    dropdown 추가

--- seller/client/src/components/layout/Navbar.js
+++ seller/client/src/components/layout/Navbar.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className='nav-wrap'>
+      <div className='Lnav-wrap'>
+        <div>상품제작</div>
+        <div>상품등록</div>
+        <div>판매현황</div>
+      </div>
+      <div className='Rnav-wrap'>문의하기</div>
+    </div>
+  );
 }
 
 export default Navbar;
--- seller/client/src/components/pages/HelpToAdmin.js
+++ seller/client/src/components/pages/HelpToAdmin.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React from 'react';
+import "../css/pages/HelpToAdmin.css";
+import Dropdown from '../function/Dropdown';
 
-function Help() {
-  return <div>
-    <h1>관리자 문의</h1>
-    <div></div>
-    <div></div>
-  </div>;
-}
+const Help = props => {
+    const [dropdownVisibility, setDropdownVisibility] = React.useState(false);
+
+    return (
+        <div id='app'>
+            <button onClick={e => setDropdownVisibility(!dropdownVisibility)}>
+                {
+                    dropdownVisibility
+                        ? 'Close'
+                        : 'Open'
+                }
+            </button>
+            <Dropdown visibility={dropdownVisibility}>
+                <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                </ul>
+            </Dropdown>
+        </div>
+    )
+};
 
 export default Help;
