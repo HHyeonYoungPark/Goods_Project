fbb857230976511e4597c03f05a69589c28e235b
Podole2 <aluvub@gmail.com>
2022-11-08 12:04:38 +0900

    11-8

--- customer/client/src/components/layout/Header.js
+++ customer/client/src/components/layout/Header.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/layout/Header.css";
 
 function Header() {
-    return (
-        <div>
-            <></>Header
+  const [search, setSearch] = useState("");
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="HeaderContainer">
+        <div className="NavMenuLeft">
+          <Link to="/seller" className="toSellerMain">
+            WE'TINY Business
+          </Link>
         </div>
-    );
+        <div className="HeaderMenuRight">
+          <ul>
+            <li>
+              <Link to="/customerHelp" className="customerHelp">
+                customerHelp
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" className="profile">
+                profile
+              </Link>
+            </li>
+            <li>
+              <Link to="/toggle" className="toggle">
+                toggle
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- customer/client/src/components/layout/Navbar.js
+++ customer/client/src/components/layout/Navbar.js
@@ -1,45 +1,57 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import wetinyWordLogo from "../images/wetiny_wordlogo.png";
+import "../css/layout/Navbar.css";
 
 function Navbar() {
-    const [search, setSearch] = useState("");
-    const onChange = (e) => {
-        setSearch(e.target.value);
-    };
+  const [search, setSearch] = useState("");
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-    return (
-        <div>
-            <div className="container">
-                <div className="NavMenuLeft">
-                    <Link to="/" className="tomain">
-                        <img src={wetinyWordLogo} alt={wetinyWordLogo} />
-                    </Link>
-                    <Link to="/influencer" className="influencer">
-                        influencer
-                    </Link>
-                    <Link to="/goods" className="goods">
-                        goods
-                    </Link>
-                </div>
-                <div className="NavMenuRight">
-                    <input type="text" value={search} onChange={onChange} />
-                    <Link to="/wishList" className="wishList">
-                        wishList
-                    </Link>
-                    <Link to="/shoppingBag" className="shoppingBag">
-                        shoppingBag
-                    </Link>
-                    <Link to="/profile" className="profile">
-                        profile
-                    </Link>
-                    <Link to="/toggle" className="toggle">
-                        toggle
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="NavbarContainer">
+        <div className="NavMenuLeft">
+          <Link to="/" className="tomain">
+            <img src={wetinyWordLogo} alt={wetinyWordLogo} />
+          </Link>
         </div>
-    );
+        <div className="NavMenuMid">
+          <ul>
+            <li>
+              <Link to="/influencer" className="influencer">
+                influencer
+              </Link>
+            </li>
+            <li>
+              <Link to="/goods" className="goods">
+                goods
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="NavMenuRight">
+          <ul>
+            <li>
+              <input type="text" value={search} onChange={onChange} />
+            </li>
+            <li>
+              <Link to="/wishList" className="wishList">
+                wishList
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart" className="cart">
+                cart
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
