7d1a3ed6a779d307232edbb7f625ce21b05008c5
Hyeonyoung Park <aud8637282@naver.com>
2022-11-16 13:00:32 +0900

    pages folder

--- seller/client/src/components/pages/board/Board.js
+++ seller/client/src/components/pages/board/Board.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link,useParams } from "react-router-dom";
 
 const Board = () => {
   const [lists, setLists] = useState([]);
 
   const { boardName } = useParams();
-  console.log(boardName);
+  // console.log(boardName);
   const getLists = async () => {
-    await axios
-      .get("http://localhost:4001/board?boardName=" + boardName)
-      .then((res) => {
+    await axios.get("http://localhost:4001/board?boardName="+boardName)
+      .then((res) =>{
         setLists(res.data);
       });
-  };
+  }
 
   useEffect(() => {
     getLists();
@@ -21,7 +20,7 @@ const Board = () => {
 
   return (
     <div>
-      <Link to={"/write"}>게시글 작성</Link>
+      <Link to={"/board/"+boardName+"/write"}>게시글 작성</Link>
       <table>
         <thead>
           <tr>
@@ -34,30 +33,24 @@ const Board = () => {
           </tr>
         </thead>
         <tbody>
-          {lists.map((list, key) => {
-            return (
-              <tr key={key}>
-                <td>{list.idx}</td>
-                <td>
-                  <Link
-                    to={
-                      "/board?boardName=" + boardName + "&list?idx=" + list.idx
-                    }
-                  >
-                    {list.title}
-                  </Link>
-                </td>
-                <td>{list.writer}</td>
-                <td>{list.count}</td>
-                <td>{list.regdate}</td>
-                <td>수정/삭제</td>
-              </tr>
-            );
-          })}
+          {
+            lists.map((list, key) => {
+              return(
+                <tr key={key}>
+                  <td>{list.idx}</td>
+                  <td><Link to={"/board/"+boardName+"/"+list.idx}>{list.title}</Link></td>
+                  <td>{list.writer}</td>
+                  <td>{list.count}</td>
+                  <td>{list.regdate}</td>
+                  <td>수정/삭제</td>
+                </tr>
+              );
+            })
+          }
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default Board;
+export default Board
--- seller/client/src/components/pages/board/BoardAdd.js
+++ seller/client/src/components/pages/board/BoardAdd.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -20,108 +20,35 @@ const BoardAdd = () => {
 
   async function frmHandler(e) {
     e.preventDefault();
-    const data = {};
-    await axios
-      .post("http://localhost:4001/boardAdd", data)
+    const data = {}
+    await axios.post("http://localhost:4001/boardAdd", data)
       .then((response) => {
-        if (response.data.status === 201) {
+        if(response.data.status === 201){
           window.alert(response.data.message);
-          navigate("/");
+          navigate("/boardManager");
         }
-      });
+      })
   }
 
   return (
     <div>
       <form method="post" className="frm" onSubmit={frmHandler}>
-        boardName
-        <input
-          type="text"
-          className="boardName"
-          onChange={(e) => setBoardName(e.target.value)}
-        />
-        <br />
-        boardType
-        <input
-          type="text"
-          className="boardType"
-          onChange={(e) => setBoardType(e.target.value)}
-        />
-        <br />
-        boardUrl
-        <input
-          type="text"
-          className="boardUrl"
-          onChange={(e) => setBoardUrl(e.target.value)}
-        />
-        <br />
-        secret
-        <input
-          type="text"
-          className="secret"
-          onChange={(e) => setSecret(e.target.value)}
-        />
-        <br />
-        readAllow
-        <input
-          type="text"
-          className="readAllow"
-          onChange={(e) => setReadAllow(e.target.value)}
-        />
-        <br />
-        writeAllow
-        <input
-          type="text"
-          className="writeAllow"
-          onChange={(e) => setWriteAllow(e.target.value)}
-        />
-        <br />
-        replyAllow
-        <input
-          type="text"
-          className="replyAllow"
-          onChange={(e) => setReplyAllow(e.target.value)}
-        />
-        <br />
-        modifyAllow
-        <input
-          type="text"
-          className="modifyAllow"
-          onChange={(e) => setModifyAllow(e.target.value)}
-        />
-        <br />
-        deleteAllow
-        <input
-          type="text"
-          className="deleteAllow"
-          onChange={(e) => setDeleteAllow(e.target.value)}
-        />
-        <br />
-        upload
-        <input
-          type="text"
-          className="upload"
-          onChange={(e) => setUpload(e.target.value)}
-        />
-        <br />
-        download
-        <input
-          type="text"
-          className="download"
-          onChange={(e) => setDownload(e.target.value)}
-        />
-        <br />
-        boardDesc
-        <input
-          type="text"
-          className="boardDesc"
-          onChange={(e) => setBoardDesc(e.target.value)}
-        />
-        <br />
+        boardName<input type="text" className="boardName" onChange={(e) => setBoardName(e.target.value)}/><br/>
+        boardType<input type="text" className="boardType" onChange={(e) => setBoardType(e.target.value)}/><br/>
+        boardUrl<input type="text" className="boardUrl" onChange={(e) => setBoardUrl(e.target.value)}/><br/>
+        secret<input type="text" className="secret" onChange={(e) => setSecret(e.target.value)}/><br/>
+        readAllow<input type="text" className="readAllow" onChange={(e) => setReadAllow(e.target.value)}/><br/>
+        writeAllow<input type="text" className="writeAllow" onChange={(e) => setWriteAllow(e.target.value)}/><br/>
+        replyAllow<input type="text" className="replyAllow" onChange={(e) => setReplyAllow(e.target.value)}/><br/>
+        modifyAllow<input type="text" className="modifyAllow" onChange={(e) => setModifyAllow(e.target.value)}/><br/>
+        deleteAllow<input type="text" className="deleteAllow" onChange={(e) => setDeleteAllow(e.target.value)}/><br/>
+        upload<input type="text" className="upload" onChange={(e) => setUpload(e.target.value)}/><br/>
+        download<input type="text" className="download" onChange={(e) => setDownload(e.target.value)}/><br/>
+        boardDesc<input type="text" className="boardDesc" onChange={(e) => setBoardDesc(e.target.value)}/><br/>
         <input type="submit" value="Create" />
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default BoardAdd;
+export default BoardAdd
