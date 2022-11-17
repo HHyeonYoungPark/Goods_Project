f973d5d69f1dbb4b565a0d2bbf3774fbff3bcbf2
sauring3 <jeonghunhijohn@gmail.com>
2022-11-17 09:11:36 +0900

    1117

---
+++ seller/client/src/components/pages/goods/Pay.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../css/pages/Notice.css";
+
+function Pay(){
+  return(
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Pay;
