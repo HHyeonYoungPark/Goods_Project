d1facb63c9f9087af636ce0f18ada539aae25d8a
sauring3 <jeonghunhijohn@gmail.com>
2022-11-18 14:53:09 +0900

    mmm

--- seller/client/src/components/pages/goods/TableGallary.js
+++ seller/client/src/components/pages/goods/TableGallary.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "../../css/pages/TableGallary.css";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React from 'react';
+import '../../css/pages/TableGallary.css';
+import { useNavigate, Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function TableGallary() {
   const [items, setItems] = useState([]);
 
   async function getAllItem() {
-    await axios.get("http://localhost:4001/goodsManager").then((response) => {
+    await axios.get('http://localhost:4001/goodsManager').then((response) => {
       setItems(response.data);
     });
   }
@@ -16,24 +16,33 @@ function TableGallary() {
   useEffect(() => {
     getAllItem();
   }, []);
+  const navigate = useNavigate();
+
+  // async function detailItem(idx) {
+  //   await axios.get('http://localhost:4001/detail/' + idx).then((response) => {
+  //     if (response.data.status === 201) {
+  //       navigate('/detail/' + idx);
+  //     }
+  //   });
+  // }
 
   return (
-    <div className="TableGallary-container">
+    <div className='TableGallary-container'>
       {items.map((item, key) => {
         return (
-          <div className="TableGallary" key={key}>
-            <Link to="#">
+          <div className='TableGallary' key={key}>
+            <Link to='/detail'>
               <img
-                style={{ width: "100%" }}
+                style={{ width: '100%' }}
                 src={`http://localhost:4001/${item.attach}`}
                 alt={item.attach}
               />
             </Link>
             <h4>
-              <Link to="#">{item.itemname}</Link>
+              <Link to={`/detail/${item.idx}`}>{item.itemname}</Link>
             </h4>
-            <p className="price">
-              <Link to="#">{item.price}</Link>
+            <p className='price'>
+              <Link to='#'>{item.price}</Link>
             </p>
           </div>
         );
