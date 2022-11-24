0dc13c09c2b1e85171f5610286bd571d844373cd
Hyeonyoung Park <aud8637282@naver.com>
2022-11-24 15:22:48 +0900

    결제페이지 생성

--- seller/client/src/components/layout/Footer.js
+++ seller/client/src/components/layout/Footer.js
@@ -21,15 +21,11 @@ function Footer() {
           <Link>사이트맵</Link>
         </div>
         <div className="coryright-addr">
-          <p>
-            (51154) 경상남도 창원시 의창구 중앙대로 300(사림동) 경남도청
-            관광진흥과 [전화] 055-211-7760,
-          </p>
-          <p>가야문화유산과 [전화] 055-211-4581,</p>
-          <p>건축주택과 [전화] 055-211-4311</p>
+          <p>(18356) 부산광역시 해운대구 본사 [전화] 055-222-2222,</p>
+          <p>고객만족센터 [전화] 055-222-4581,</p>
+          <p>비지니스문의 [전화] 055-222-4582</p>
           <p style={{ color: "#a7a2a4" }}>
-            Copyright(c) 2018 The province of gyeongsangnam-do. All rights
-            reserved.
+            Copyright(c) 2018 The province of WE'TINY. All rights reserved.
           </p>
         </div>
       </div>
